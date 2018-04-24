d67ae73d0d9fe9983e5131271e8ffff5c729bc80
Collins Muiruri <wanyekicollins@gmail.com>
2018-04-24 17:07:49 +0300

    Update the imports

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,5 @@
 from flask import render_template, request, redirect, url_for, abort
-from . import auth
-from flask_login import login_required,current_user,login_user,logout_user
-from ..models import User
-from .forms import RegistrationForm,LoginForm
+from . import main
+from flask_login import login_required,current_user
+from ..models import User,Blog
 from .. import db
