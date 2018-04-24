2b4b1b4d5e70b810a34aa381528f4f302d7ac0ae
Collins Muiruri <wanyekicollins@gmail.com>
2018-04-24 17:09:20 +0300

    Update the imports

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,7 @@
 from flask import render_template, request, redirect, url_for, abort
 from . import main
 from flask_login import login_required,current_user
-from ..models import User,Blog,Comment,Role
-from .forms import BlogForm,CommentForm,DeleteBlog
+from ..models import User,Blog
 from .. import db
 
 @main.route('/')
