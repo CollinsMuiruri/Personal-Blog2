678f58a6e04b5c74cec2042353c73f5c27e671f1
Collins Muiruri <wanyekicollins@gmail.com>
2018-04-24 17:02:16 +0300

    Add the blog class

--- app/models.py
+++ app/models.py
@@ -32,3 +32,15 @@ class User(UserMixin, db.Model):
 
          def verify_password(self,password):
              return check_password_hash(self.pass_secure,password)
+
+
+class Blog(db.Model):
+
+   __tablename__ = 'blog'
+
+   id = db.Column(db.Integer, primary_key=True)
+   title = db.Column(db.String(50))
+   subtitle = db.Column(db.String(50))
+   author = db.Column(db.String(20))
+   date_posted = db.Column(db.DateTime, default=datetime.utcnow)
+   content = db.Column(db.Text)
