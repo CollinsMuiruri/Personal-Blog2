066c2e59fc208e505d98212577a7eac9cded0280
Collins Muiruri <wanyekicollins@gmail.com>
2018-04-25 08:50:09 +0300

    Add user database to admin view

--- app/models.py
+++ app/models.py
@@ -14,31 +14,28 @@ def load_user(user_id):
 
 
 class User(UserMixin, db.Model):
- __tablename__ = 'users'
- id =db.Column(db.Integer,primary_key =True)
- username = db.Column(db.String(255),index = True)
- email = db.Column(db.String(255),unique = True,index = True)
- role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
- password_hash =db.Column(db.String(255))
+    __tablename__ = 'users'
+    id =db.Column(db.Integer,primary_key =True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    pass_secure  = db.Column(db.String(255))
 
- pass_secure  = db.Column(db.String(255))
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
 
- @property
- def password(self):
-     raise AttributeError('You cannot read the password attribute')
-
-     @password.setter
-     def password(self, password):
-         self.pass_secure = generate_password_hash(password)
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
 
 
-         def verify_password(self,password):
-             return check_password_hash(self.pass_secure,password)
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
 
 
 class Blog(db.Model):
 
-   __tablename__ = 'blog'
+   __tablename__ = 'blogs'
 
    id = db.Column(db.Integer, primary_key=True)
    title = db.Column(db.String(50))
@@ -46,3 +43,5 @@ class Blog(db.Model):
    author = db.Column(db.String(20))
    date_posted = db.Column(db.DateTime, default=datetime.utcnow)
    content = db.Column(db.Text)
+
+admin.add_view(ModelView(User,db.session))
