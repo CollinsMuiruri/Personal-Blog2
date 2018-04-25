f7a87da0eb9f3e86af8d8416ad36e2ac954e8731
Collins Muiruri <wanyekicollins@gmail.com>
2018-04-25 08:47:33 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,19 +2,30 @@ import os
 
 
 class Config:
-    SECRET_KEY=os.environ.get('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # simple mde configuration
     SIMPLEMDE_JS_FILE = True
     SIMPLEMDE_USE_CDN = True
+    UPLOADED_PHOTOS_DEST  ='app/static/photos'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://collins:wildgoosechase@localhost/blogs'
+    SECRET_KEY="0717745223"
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
-
+    @staticmethod
+    def init_app(app):
+        pass
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    pass
 
 class DevConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://collins:wildgoosechase@localhost/blog'
 
-config_options ={"production":ProdConfig,"development":DevConfig}
+config_options ={
+"production":ProdConfig,
+"development":DevConfig
+}
