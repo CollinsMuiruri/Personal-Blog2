cce060167e781afcded2d6e4d3d186502d5445aa
Collins Muiruri <wanyekicollins@gmail.com>
2018-04-25 08:49:02 +0300

    Add admin imports

--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
-from . import db
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from . import db,admin,login_manager
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
-from . import  login_manager
+from datetime import datetime
 
 
 
